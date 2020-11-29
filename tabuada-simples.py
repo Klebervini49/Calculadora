d24aab5fa18ed880a34c2be4055d255a857b0935
Kleberson Vinicius <klebervini49@gmail.com>
2020-11-29 16:42:52 -0300

    Add files via upload

---
+++ tabuada-simples.py
@@ -0,0 +1,26 @@
+print('Tabuada')
+
+print('')
+numero = int(input('Digite um número\n> '))
+# Começo
+n0: int = numero * 1
+n1 = numero * 2
+n2 = numero * 3
+n3 = numero * 4
+n4 = numero * 5
+n5 = numero * 6
+n6 = numero * 7
+n7 = numero * 8
+n8 = numero * 9
+n9 = numero * 10
+# Fim
+print('1 - {}\n'
+      '2 - {}\n'
+      '3 - {}\n'
+      '4 - {}\n'
+      '5 - {}\n'
+      '6 - {}\n'
+      '7 - {}\n'
+      '8 - {}\n'
+      '9 - {}\n'
+      '10 - {}\n'.format(n0, n1, n2, n3, n4, n5, n6, n7, n8, n9))
